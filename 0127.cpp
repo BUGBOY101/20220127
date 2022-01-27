ff31ae7ea38247b94ce445d55b70d1e148482a00
BUG <2251093140@qq.com>
2022-01-27 21:04:10 +0800

    添加项目文件。

---
+++ 0127.cpp
@@ -0,0 +1,41 @@
+#define	_CRT_SECURE_NO_WARNINGS 1
+
+#include<iostream>
+using namespace std;
+int main() {
+	char c;
+	cin >> c;
+	cout << "  " << c << endl;
+	cout << " " << c << c << c << endl;
+	cout << c << c << c << c << c << endl;
+}
+
+//#include<iostream>
+//using namespace std;
+//int main() {
+//	int a[3], i;
+//	for (i = 0; i < 3; i++)
+//		cin >> a[i];
+//	for (i = 0; i < 3; i++) {
+//		if (i != 2) {
+//			printf("%8d ", a[i]);
+//		}
+//		else {
+//			printf("%8d\n", a[i]);
+//		}
+//	}
+//}
+
+//#include<iostream>
+//using namespace std;
+//int main() {
+//	cout << "Hello,World!" << endl;
+//}
+
+//#include<iostream>
+//using namespace std;
+//int main() {
+//	long a, b;
+//	cin >> a >> b;
+//	cout << a + b << endl;
+//}
